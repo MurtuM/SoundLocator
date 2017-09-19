115dc249f7dcd8cc6d6c44fc99519fb8ac1f6a20
Rikk666 <32096222+Rikk666@users.noreply.github.com>
2017-09-19 23:19:34 +0200

    Added scircular lisat and channel

---
+++ SoundLocator/SoundLocator/Channel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundLocator
+{
+    class Channel
+    {
+        private CircularList m_short = new CircularList(60); // 60 frames = 1 sec
+        private CircularList m_medium = new CircularList(10); // 60 * 10 = 10 sec
+        private CircularList m_long = new CircularList(10);  // 60 * 10 * 10 = 100 sec
+
+        int m_medium_counter = 0;
+        int m_long_counter = 0;
+
+        float m_short_avg = -1;
+        float m_medium_avg = -1;
+        float m_long_avg = -1;
+
+        private float CalcAvg(CircularList list)
+        {
+            float avg = 0;
+            foreach (float v in list)
+                avg += v;
+            avg /= list.Count;
+            return avg;
+        }
+
+        public void AddVolume(float value)
+        {
+            m_short.Add(value);
+            m_short_avg = -1;
+            if ((m_medium_counter % 10) == 0 || m_medium_counter < 10)
+            {
+                m_short_avg = CalcAvg(m_short);
+                m_medium.Add(m_short_avg);
+
+                if ((m_long_counter % 10) == 0 || m_long_counter < 10)
+                {
+                    m_medium_avg = CalcAvg(m_medium);                    
+                    m_long.Add(m_medium_avg);
+
+                    m_long_avg = CalcAvg(m_long);
+                }
+                m_long_counter++;
+            }
+            m_medium_counter++;
+        }
+
+        public float ShortAverage
+        {
+            get
+            {
+                if (m_short_avg < 0)
+                    m_short_avg = CalcAvg(m_short);
+                return m_short_avg;
+            }
+        }
+
+        public float MediumAverage
+        {
+            get
+            {
+                if (m_medium_avg < 0)
+                    m_medium_avg = CalcAvg(m_medium);
+                return m_medium_avg;
+            }
+        }
+
+        public float LongAverage
+        {
+            get
+            {
+                if (m_long_avg < 0)
+                    m_long_avg = CalcAvg(m_long);
+                return m_long_avg;
+            }
+        }
+    }
+}
---
+++ SoundLocator/SoundLocator/CircularList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundLocator
+{
+    class CLInumerator : System.Collections.IEnumerator
+    {
+        int m_index = -1;
+        CircularList m_list;
+        public CLInumerator(CircularList list)
+        {
+            m_list = list;
+        }
+
+        public bool MoveNext()
+        {
+            m_index++;
+            return (m_index < m_list.Count);
+        }
+
+        public void Reset()
+        { m_index = -1; }
+
+        public object Current
+        {
+            get { return m_list[m_index]; }
+        }
+    };
+
+    class CircularList : System.Collections.IEnumerable
+    {
+        float[] m_list = null;
+        int m_next = 0;
+        int m_count = 0;
+        int m_size = 0;
+
+        public CircularList(int size)
+        {
+            m_list = new float[size];
+            m_size = size;
+        }
+        public int Count
+        {
+            get { return m_count; }
+        }
+        public float this[int i]
+        {
+            get
+            {
+                if (i > m_count || i < 0)
+                    throw new IndexOutOfRangeException();
+                if (m_count == m_size)
+                    return m_list[(m_next + i) % m_size];
+                else
+                    return m_list[i];
+            }
+            set
+            {
+                if (i > m_count || i < 0)
+                    throw new IndexOutOfRangeException();
+                if (m_count == m_size)
+                    m_list[(m_next + i) % m_size] = value;
+                else
+                    m_list[i] = value;
+            }
+        }
+        public void Add(float value)
+        {
+            m_count = Math.Min(m_count + 1, m_size);
+            m_list[m_next] = value;
+            m_next = (m_next + 1) % m_size;
+        }
+
+        public System.Collections.IEnumerator GetEnumerator()
+        {
+            return new CLInumerator(this);
+        }
+    }
+}
